7e8c8c195d1f18b8b0b670e9ec8b50b4afed9397
iMacpay <66511553+iMacpay@users.noreply.github.com>
2021-09-16 10:49:49 +0800

    add ThreadPool并发

--- proxyfilter.py
+++ proxyfilter.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 
@@ -15,15 +16,32 @@ def simplecheck(Testurl, Proxy):
     r = requests.session()
     proxies = {'http': 'http://' + Proxy,
                'https': 'http://' + Proxy}
-    r = requests.get(Testurl, proxies=proxies)
+    try:
+        r = requests.get(Testurl, proxies=proxies,timeout=10)
+    except requests.exceptions.ProxyError:
+        print(f'Proxy {Proxy} Can not connect')
     result = r.status_code
-    return result
+    return result,Proxy
+
+
 
 
 if __name__ == "__main__":
-    testurl = ""
-    proxyurl = ""
+    testurl = "http://proxytest.edatastore.io"
+    proxyurl = "http://onezpay.com/iplist/txt"
     proxylist = getproxy(proxyurl)
-    for proxy in proxylist:
-        res = simplecheck(testurl, proxy)
-        print(proxy, "response code is:", res)
+    Threads=50
+    with ThreadPoolExecutor(max_workers=Threads) as executor:
+        obj_list = []
+        begin = time.time()
+        for proxy in proxylist:
+            res = executor.submit(simplecheck,testurl,proxy)
+            obj_list.append(res)
+        for future in as_completed(obj_list):
+            data = future.result()
+            print(f'the response from proxy :{data[1]}  is  {data[0]}！')
+        times = time.time() - begin
+        print(times)
+
+
+
